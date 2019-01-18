aea07b9ae0ceba707ddfe3199a064be5920b829c
Shin'ya UEOKA <ueokande@i-beam.org>
2019-01-18 03:31:45 +0000

    Enable npm run build

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'build');
+
+config = {
+  entry: {
+    content: path.join(src, 'content'),
+    background: path.join(src, 'background'),
+  },
+  output: {
+    path: dist,
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: [ /\.js$/ ],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['stage-2']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.js' ],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+  },
+};
+
+module.exports = config
