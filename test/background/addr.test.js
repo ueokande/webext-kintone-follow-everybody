33b6a6662bb5d123a847d857435003a24249acd1
Shin'ya UEOKA <ueokande@i-beam.org>
2019-01-18 03:58:40 +0000

    Enable npm run eslint

--- test/background/addr.test.js
+++ test/background/addr.test.js
@@ -3,6 +3,6 @@ import Addr from '../../src/background/addr'
 describe('Addr', () => {
   it('adds a number', () => {
     let addr = new Addr();
-    expect(addr.add(10, 20)).to.equal(30)
+    expect(addr.add(10, 20)).to.equal(30);
   })
 })
